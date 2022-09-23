6bf55517f3af08bc985f3aa69c271f192ffe92d8
Даниил Лямин <Niger120404@yandex.ru>
2022-09-23 18:43:15 +0300

    +

--- LaboratoryWorkOnDataBases/Context.cs
+++ LaboratoryWorkOnDataBases/Context.cs
@@ -4,19 +4,19 @@ namespace LaboratoryWorkOnDataBases
 {
 	internal class Context : DbContext
 	{
-		public virtual DbSet<ConstructionCompany> ConstructionCompanies { get; set; } = null!;
+		public DbSet<ConstructionCompany> ConstructionCompanies { get; set; } = null!;
 
-		public virtual DbSet<ConstructionRepair> ConstructionRepairs { get; set; } = null!;
+		public DbSet<ConstructionRepair> ConstructionRepairs { get; set; } = null!;
 
-		public virtual DbSet<Customer> Customers { get; set; } = null!;
+		public DbSet<Customer> Customers { get; set; } = null!;
 
-		public virtual DbSet<Order> Orders { get; set; } = null!;
+		public DbSet<Order> Orders { get; set; } = null!;
 
-		public virtual DbSet<RepairInvoice> RepairInvoices { get; set; } = null!;
+		public DbSet<RepairInvoice> RepairInvoices { get; set; } = null!;
 
-		public virtual DbSet<TeamOfWorker> TeamOfWorkers { get; set; } = null!;
+		public DbSet<TeamOfWorker> TeamOfWorkers { get; set; } = null!;
 
-		public virtual DbSet<Worker> Workers { get; set; } = null!;
+		public DbSet<Worker> Workers { get; set; } = null!;
 
 		public Context()
 		{
