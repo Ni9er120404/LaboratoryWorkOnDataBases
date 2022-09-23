4a5943679635bcd3daa17feac956c46321508f39
Даниил Лямин <Niger120404@yandex.ru>
2022-09-23 18:35:55 +0300

    Исправлены связи

--- LaboratoryWorkOnDataBases/Context.cs
+++ LaboratoryWorkOnDataBases/Context.cs
@@ -20,13 +20,57 @@ namespace LaboratoryWorkOnDataBases
 
 		public Context()
 		{
-			_ = Database.EnsureDeleted();
-			_ = Database.EnsureCreated();
+			Database.EnsureDeleted();
+			Database.EnsureCreated();
 		}
 
 		protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
 		{
-			_ = optionsBuilder.UseSqlServer(@"Server=(localdb)\mssqllocaldb;Database=LaboratoryWorkOnDataBases;Trusted_Connection=True;");
+			optionsBuilder.UseSqlServer(@"Server=(localdb)\mssqllocaldb;Database=LaboratoryWorkOnDataBases;Trusted_Connection=True;");
+		}
+
+		protected override void OnModelCreating(ModelBuilder modelBuilder)
+		{
+			modelBuilder.Entity<ConstructionRepair>(entity =>
+			{
+				entity.HasOne(d => d.IdNavigation).WithOne(p => p.ConstructionRepair).HasForeignKey<ConstructionRepair>(d => d.Id);
+			});
+
+			modelBuilder.Entity<Customer>(entity =>
+			{
+				entity.HasOne(d => d.IdNavigation).WithOne(p => p.Customer).HasForeignKey<Customer>(d => d.Id);
+			});
+
+			modelBuilder.Entity<Order>(entity =>
+			{
+				entity.Property(e => e.Id).ValueGeneratedOnAdd().HasColumnOrder(0);
+				entity.Property(e => e.DateTime).HasColumnOrder(1);
+				entity.HasOne(d => d.IdNavigation).WithOne(p => p.Order).HasForeignKey<Order>(d => d.Id);
+			});
+
+			modelBuilder.Entity<RepairInvoice>(entity =>
+			{
+				entity.Property(e => e.Id).ValueGeneratedOnAdd().HasColumnOrder(0);
+				entity.Property(e => e.Cost).HasColumnOrder(2).HasColumnType("decimal(18, 2)");
+				entity.Property(e => e.DateTime).HasColumnOrder(1);
+				entity.HasOne(d => d.IdNavigation).WithOne(p => p.RepairInvoice).HasForeignKey<RepairInvoice>(d => d.Id);
+			});
+
+			modelBuilder.Entity<TeamOfWorker>(entity =>
+			{
+				entity.Property(e => e.Id).ValueGeneratedOnAdd().HasColumnOrder(0);
+				entity.HasOne(d => d.IdNavigation).WithOne(p => p.TeamOfWorker).HasForeignKey<TeamOfWorker>(d => d.Id);
+			});
+
+			modelBuilder.Entity<Worker>(entity =>
+			{
+				entity.Property(e => e.Id).ValueGeneratedOnAdd().HasColumnOrder(0);
+				entity.Property(e => e.FirstName).HasColumnOrder(3);
+				entity.Property(e => e.LastName).HasColumnOrder(2);
+				entity.Property(e => e.PhoneNumber).HasColumnOrder(4);
+				entity.Property(e => e.Salary).HasColumnOrder(1).HasColumnType("decimal(18, 2)");
+				entity.HasOne(d => d.IdNavigation).WithOne(p => p.Worker).HasForeignKey<Worker>(d => d.Id);
+			});
 		}
 	}
 }
